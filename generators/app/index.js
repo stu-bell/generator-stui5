3eaaafa8a102fd0838b838342f18e4ab996e96ed
Stuart Bell <stuart.bell@bluefinsolutions.com>
2017-07-18 14:21:57 +0100

    add prompts

--- generators/app/index.js
+++ generators/app/index.js
@@ -9,6 +9,26 @@ module.exports = class extends Generator {
 		this.option('lint');
 	}
 
+	prompting() {
+		// prompts returned as a promise
+		return this.prompt([
+			{
+				type: 'input',
+				name: 'name',
+				message: 'What\'s your project name?',
+				default: this.appname //default to current folder name
+			},
+			{
+				type: 'confirm',
+				name: 'lint',
+				message: 'Would you like to include .eslintrc?'
+			}
+		]).then((responses) => {
+			this.log('Name:', responses.name);
+			this.log('Include .eslintrc:', responses.lint);
+		});
+	}
+
 	method1() {
 		this.log('method1 just ran :D');
 	}
