aa669cfdb7bb09696f85c0109edddd7e3075225a
Stuart Bell <stuart.bell@bluefinsolutions.com>
2017-07-18 13:42:23 +0100

    add constructor

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,6 +1,12 @@
 var Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
+	constructor(args, opts) {
+		// call super constructor
+		super(args, opts);
 
-	
+		// add flags
+		this.option('lint'); 
+	}
+
 };
