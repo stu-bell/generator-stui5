35922384ebb06afcf0392e2886599787a9867530
Stuart Bell <stuart.bell@bluefinsolutions.com>
2017-07-19 12:48:13 +0100

    yo stui5:config prompts only for missing params

--- generators/app/index.js
+++ generators/app/index.js
@@ -16,27 +16,8 @@ module.exports = class extends Generator {
 		// TODO: build array of prompts for mandatory parmaters not present on the config
 		// TODO: prompt: would you like to edit the config file before proceding? default No. If yes, edit rc then rerun generator.
 
-		var aPrompts = [
-			{
-				type: 'input',
-				name: 'name',
-				message: 'What\'s your project name?',
-				default: this.appname //default to current folder name
-			},
-			{
-				type: 'input',
-				name: 'namespace',
-				message: 'What\'s your project namespace?',
-				default: 'scb.yeo' // TODO: rename to slug of project? R.slugify?
-			},
-			{
-				type: 'confirm',
-				name: 'lint',
-				message: 'Would you like to include .eslintrc?'
-			}
-		];
+		var aPrompts = [];
 
-		
 
 		// prompt returned as a promise
 		return this.prompt(aPrompts).then((responses) => {
--- generators/config/index.js
+++ generators/config/index.js
@@ -2,20 +2,52 @@ var Generator = require('yeoman-generator'),
 R = require('ramda');
 
 module.exports = class extends Generator {
-		initializing(){
-			// set default config
-			this.config.defaults({
-				eslint: true,
-				rootViewName: "app.view.xml"
-			});
-			this.log('Full config available in .yo-rc.json')
-		}
+	initializing(){
+		// set default config
+		this.config.defaults({
+			eslint: true,
+			rootViewName: "app.view.xml"
+		});
+		this.log('Full config available in .yo-rc.json')
+	}
 
-		prompting(){
-			// figuring out how to check for existing config keys
-			var isConfigNil = sKey => R.isNil(this.config.get(sKey));
-			this.log('name param: ', this.config.get('name'));
-			this.log('isnil: ', isConfigNil('name'));
-		}
+	prompting(){
+		// check if a config key has a value
+		var isConfigNil = sKey => R.isNil(this.config.get(sKey));
+		// check if a prompt is required (not required if the prompt is already in config)
+		var isPromptReq = R.pipe(R.prop('name'), isConfigNil);
+
+		var aPrompts = [
+			{
+				type: 'input',
+				name: 'name',
+				message: 'What\'s your project name?',
+				default: this.appname //default to current folder name
+			},
+			{
+				type: 'input',
+				name: 'namespace',
+				message: 'What\'s your project namespace?',
+				default: 'scb.yeo' // TODO: rename to slug of project? R.slugify?
+			},
+			{
+				type: 'confirm',
+				name: 'eslint',
+				message: 'Would you like to include .eslintrc?'
+			}
+		];
+
+
+		return this.prompt(R.filter(isPromptReq, aPrompts))
+		.then((responses) => {
+			// TODO: save to config.
+
+			this.log('name: ', this.config.get('name') );
+			this.log('namespace: ', responses.namespace);
+			this.log('eslint: ', this.config.get('eslint'));
+		});
+
+
+	}
 
 };
